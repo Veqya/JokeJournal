733a6dbf67581478783c03417474414e4ae47239
Vemir <52715044+Veqya@users.noreply.github.com>
2023-09-02 00:23:09 +0400

    Added getRandomJokeRepo testing.

---
+++ data/src/test/kotlin/com/jokejournal/data/GetLocalJokeByIdUseCaseImplTest.kt
@@ -0,0 +1,39 @@
+package com.jokejournal.data
+
+import entities.remote.Joke
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import repositories.JokesRepository
+import usecases.GetLocalJokeByIdUseCaseImpl
+
+@RunWith(MockitoJUnitRunner::class)
+class GetLocalJokeByIdUseCaseImplTest {
+    @Mock
+    lateinit var jokesRepository: JokesRepository
+
+    private lateinit var useCase: GetLocalJokeByIdUseCaseImpl
+
+    @Before
+    fun setUp() {
+        useCase = GetLocalJokeByIdUseCaseImpl(jokesRepository, Dispatchers.IO)
+    }
+
+    @Test
+    fun `test get joke by ID`() = runBlocking {
+        val jokeId = 1
+        val expectedJoke = Joke(jokeId, "Test Joke Type", "Test Joke Setup", "Test Joke Punchline")
+
+        Mockito.`when`(jokesRepository.getLocalJokeById(jokeId)).thenReturn(expectedJoke)
+
+        val result = useCase.execute(jokeId)
+
+        assertEquals(expectedJoke, result)
+    }
+}
